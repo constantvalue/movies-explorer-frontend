54b864587a492690eb13c892a5af071c672387ea
Artem Voronin <rainarlp@yandex.ru>
2023-11-02 21:02:10 +0300

    feat: add <AboutMe/> adaptive layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,18 +8,22 @@ function Header() {
         <nav className="header__navigation">
           <img className="header__logo" src={logo} alt="Лого" />
           <ul className="header__navigation-container">
-            <a href="/movies" className="header__link">Фильмы</a>
-            <a href="/saved" className="header__link">Сохранённые фильмы</a>
+            <a href="/movies" className="header__link">
+              Фильмы
+            </a>
+            <a href="/saved" className="header__link">
+              Сохранённые фильмы
+            </a>
           </ul>
         </nav>
         <button className="header__account-button">
-          <a href="/profile" className="header__account-link">Аккаунт</a>
+          <a href="/profile" className="header__account-link">
+            Аккаунт
+          </a>
 
           <div className="header__account-logo" />
         </button>
-        <button className="header__account-burger">
-          
-        </button>
+        <button className="header__account-burger"></button>
       </div>
     </section>
   );
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,7 +3,7 @@ import Promo from "./Promo/Promo";
 import NavTab from "./NavTab/NavTab";
 import AboutProject from "./AboutProject/AboutProject";
 import Techs from "./Techs/Techs";
-// import AboutMe from "./AboutMe/AboutMe";
+import AboutMe from "./AboutMe/AboutMe";
 // import Portfolio from "./Portfolio/Portfolio";
 // import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
@@ -16,8 +16,8 @@ function Main() {
       <NavTab />
       <AboutProject />
       <Techs />
-      {/* <AboutMe />
-      <Portfolio />
+      <AboutMe />
+      {/* <Portfolio />
       <Footer/> */}
     </div>
   );

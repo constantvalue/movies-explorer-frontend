a8706517a64c711f11e6cdd4617d5243ae5e8d35
Artem Voronin <rainarlp@yandex.ru>
2023-11-03 22:37:09 +0300

    feat: add <Register/> layout

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,15 +5,19 @@ import { Route, Routes } from "react-router";
 import Movies from "../Movies/Movies";
 import Profile from "../Profile/Profile";
 import SavedMovies from "../SavedMovies/SavedMovies";
+import Login from "../Login/Login";
+import Register from "../Register/Register";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
-      <Route path="/movies" element={<Movies/>}/>
-      <Route path="/profile" element={<Profile/>}/>
-      <Route path="*" element={<Page404/>}/>
-      <Route path ="/saved-movies" element={<SavedMovies/>}/>
+      <Route path="/movies" element={<Movies />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="*" element={<Page404 />} />
+      <Route path="/saved-movies" element={<SavedMovies />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,64 @@
+import "./Register.css";
+import logo from "../../images/logo_header.svg";
+
+function Register() {
+  return (
+    <section className="register">
+      <div className="register__container">
+        <img src={logo} className="register__logo" alt="лого"></img>
+        <h2 className="register__greeting">Добро пожаловать!</h2>
+        <form className="register__form">
+          <ul className="register__inputs">
+            <li className="register__inputs-item">
+              <label className="register__input-label" for="name">
+                Имя
+              </label>
+              <input
+                type="text"
+                required
+                className="register__input"
+                placeholder="Виталий"
+              ></input>
+            </li>
+            <li className="register__inputs-item">
+              <label className="register__input-label" for="email">
+                E-mail
+              </label>
+              <input
+                type="email"
+                required
+                className="register__input"
+                placeholder="pochta@yandex.ru"
+              ></input>
+            </li>
+            <li className="register__inputs-item">
+              <label className="register__input-label" for="password">
+                Пароль
+              </label>
+              <input
+                type="password"
+                required
+                className="register__input register__input_error"
+                placeholder="••••••••••••••"
+              ></input>
+              <span className="register__input-span">
+                Что-то пошло не так...
+              </span>
+            </li>
+          </ul>
+          <button type="submit" className="register__submit">
+            Зарегестрироваться
+          </button>
+          <span className="register__submit-span">
+            Уже зарегистрированы?
+            <a href="/login" className="register__signin">
+              Войти
+            </a>
+          </span>
+        </form>
+      </div>
+    </section>
+  );
+}
+
+export default Register;

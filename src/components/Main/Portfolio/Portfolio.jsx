f3889bad6c63ef4949586cca1671482fdac7eaa6
Artem Voronin <rainarlp@yandex.ru>
2023-11-04 01:22:09 +0300

    fix: fix various adaptive layout bugs

--- src/components/Main/Portfolio/Portfolio.jsx
+++ src/components/Main/Portfolio/Portfolio.jsx
@@ -6,14 +6,21 @@ function Portfolio() {
     <section className="portfolio">
       <h2 className="portfolio__heading">Портфолио</h2>
       <ul className="portfolio__links">
-        <li className="portfolio__link">
-          Статичный сайт<span className="portfolio__arrow">↗</span>
+        <li className="portfolio__links-item">
+          <a target="_blank" rel="noreferrer" className="portfolio__link" href="https://github.com/constantvalue/how-to-learn">
+            Статичный сайт<span className="portfolio__arrow">↗</span>
+          </a>
         </li>
-        <li className="portfolio__link">
-          Адаптивный сайт<span className="portfolio__arrow">↗</span>
+        <li className="portfolio__links-item">
+          <a target="_blank" rel="noreferrer" className="portfolio__link" href="https://github.com/constantvalue/russian-travel">
+            Адаптивный сайт<span className="portfolio__arrow">↗</span>
+          </a>
         </li>
-        <li className="portfolio__link">
-          Одностраничное приложение<span className="portfolio__arrow">↗</span>
+
+        <li className="portfolio__links-item">
+          <a target="_blank" rel="noreferrer" className="portfolio__link" href="https://github.com/constantvalue/react-mesto-api-full-gha">
+            Одностраничное приложение<span className="portfolio__arrow">↗</span>
+          </a>
         </li>
       </ul>
     </section>

27db140952eb625d39ffaa305354297267fd9a39
Artem Voronin <rainarlp@yandex.ru>
2023-11-06 08:52:32 +0300

    fix: fix 404page adaptive layout

--- src/components/Page404/Page404.jsx
+++ src/components/Page404/Page404.jsx
@@ -6,8 +6,13 @@ function Page404() {
       <div className="page404__error-message">
         <h2 className="page404__error-code">404</h2>
         <p className="page404__error-caption">Страница не найдена</p>
+        <a href="/" className="page404__link">
+        Назад
+      </a>
       </div>
-      <a href="/" className="page404__link"> Назад</a>
+
+
+
     </section>
   );
 }
